8b5d6e53d45f9669af6184505fc28dd635d6c1a3
Utkash Dubey <utkashd@gmail.com>
2025-07-15 02:54:06 -0700

    not great not terrible: migration script infra

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Callable, Literal
 
 import uvicorn
 from api import (
@@ -12,6 +12,7 @@ from api import (
 )
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from k4_logger import log
 from utils.environment import is_development_environment, is_running_in_docker_container
 
 app = FastAPI(lifespan=lifespan)
@@ -40,6 +41,37 @@ async def am_i_alive() -> Literal[True]:
     return True
 
 
+def setup() -> None:
+    """
+    Here we run any version-to-version migrations that need to take place.
+
+    "Why don't you just use SqlAlchemy + Alembic?"
+
+    I think it's easy to forget the advantages of using raw SQL as strings:
+        - transparency: we know *exactly* what queries are being run in Postgres
+        - trading risks: raw SQL strings feel risky, but abstracted queries carry other
+          risks. And if something goes wrong, it's going to be tougher to fix issues
+          originating from bad SqlAlchemy vs. bad raw queries
+        - inertia: I'll never be able to completely forget about SQL. Might as well stay
+          sharp and embrace the low-level
+
+    All that, plus raw SQL is lightweight. ORMs are heavy and the featureset can be
+    overwhelming and thus confusing. With raw SQL, everything is deliberate, and I'm
+    forced to understand what I'm doing
+    """
+
+    MIGRATIONS: list[Callable[..., None]] = []
+
+    log.info(f"Running {len(MIGRATIONS)} migrations")
+
+    for idx, migration in enumerate(MIGRATIONS):
+        log.info(f"Starting migration {idx}: {migration.__name__}")
+        migration()
+        log.info(f"Completed migration {idx}: {migration.__name__}")
+
+    log.info(f"Finished running {len(MIGRATIONS)} migrations")
+
+
 def main() -> None:
     uvicorn.run(
         app="main:app",  # app=app
@@ -50,4 +82,5 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    setup()
     main()
